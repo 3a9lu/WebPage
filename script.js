882948c52fb0a181859d89327ab58de7dac2bf3e
Eduard <eduard.grinvald@yandex.ru>
2022-11-14 17:43:27 +0700

    Lab7: готова.

---
+++ script.js
@@ -0,0 +1,33 @@
+// Задание 3. Выведите на экран любое значение
+console.log("15");
+
+// Задание 4. Создайте две переменные с числами. Выведите их на экран
+const numberOne = 10;
+const numberTwo = 6;
+console.log(numberOne);
+console.log(numberTwo);
+
+// 5 Сложите две переменные и выведите результат на экран
+console.log(numberOne + numberTwo);
+
+// Задание 6. Поищите в интернете функции для преобразования строки в верхний и нижний регистр. Выведите на экран два этих значения
+const word = "Веб-разработка";
+console.log(word.toUpperCase());
+console.log(word.toLowerCase());
+
+// Задание 7. Запишите в свой скрипт реализацию каждой базовой операции над числами. 
+// Также посмотрите в интернете как возводить число в квадрат, получать остаток от деления
+console.log(numberOne + numberTwo);
+console.log(numberOne);
+console.log(numberOne * numberTwo);
+console.log(numberOne / numberTwo);
+console.log(numberOne ** 2); // возведение в квадрат
+console.log(numberOne % numberTwo); // остаток от деления
+
+// Задание 8. Придумайте два задания и покажите преподавателю
+// 1. Создайте массиы и выведите второй элемент массива.
+let fruits = ["Яблоко", "Апельсин", "Слива"];
+console.log(fruits[1]);
+
+// 2. Вывести случайное число от 0 до 100.
+console.log("Число:", Math.round(Math.random() * 101))
